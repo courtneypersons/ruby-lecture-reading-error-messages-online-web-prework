78f41840ba020bcfe110441f6888e747205d43f4
courtney persons <personscourtney@gmail.com>
2018-09-13 20:28:36 +0000

    automatically backed up by learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -13,15 +13,14 @@ describe "Not having any errors and being all green" do
     it 'raises a SyntaxError for nonsensical code' do
       expect{
         load './lib/a_syntax_error.rb'
-      }.to_not raise_error (ZeroDivisionError)
-    end
+      }.to_not raise_error 
   end
 
   context 'TypeError' do
     it 'raises a TypeError for objects of the wrong type' do
       expect{
         load './lib/a_type_error.rb'
-      }.to_not raise_error (NameError)
+      }.to_not raise_error 
     end
   end
 
@@ -29,7 +28,7 @@ describe "Not having any errors and being all green" do
     it 'raises a ZeroDivisionError for dividing by zero' do
       expect{
         load './lib/a_division_by_zero_error.rb'
-      }.to_not raise_error (TypeError)
+      }.to_not raise_error 
     end
   end
 end
