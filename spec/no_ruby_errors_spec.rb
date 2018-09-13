4724b629e6d9723a8845da89605340d28d7f2ee9
courtney persons <personscourtney@gmail.com>
2018-09-13 20:28:42 +0000

    automatically backed up by learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -13,14 +13,14 @@ describe "Not having any errors and being all green" do
     it 'raises a SyntaxError for nonsensical code' do
       expect{
         load './lib/a_syntax_error.rb'
-      }.to_not raise_error 
+      }.to_not raise_error
   end
 
   context 'TypeError' do
     it 'raises a TypeError for objects of the wrong type' do
       expect{
         load './lib/a_type_error.rb'
-      }.to_not raise_error 
+      }.to_not raise_error
     end
   end
 
